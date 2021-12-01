8874a4535f1d1edb3594f19405d3014e06c17664
Joel Ross <joelross@uw.edu>
2021-12-01 10:31:04 -0800

    End of firebase db demo

--- react-chat/src/components/ChatPage.js
+++ react-chat/src/components/ChatPage.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
+import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database';
+
 
 import ChannelNav from './Channels';
 import { MessagePane } from './Messages';
@@ -15,21 +17,70 @@ const CHANNEL_LIST = [
   'channel-4'
 ]
 
+/*
+const react-messenger-au21-default = {
+  message: "Hello firebase",
+  post: {
+    message: "I'm the first post"
+  },
+  allPost: {
+    Mpr: {1} //delete 5g
+    Mpy: {2} //change element 5l
+    Mpz: {3}
+  }
+}
+*/
+
+
+
 export default function ChatPage(props) {
   const [messageArray, setMessageArray] = useState(CHAT_LOG) //store prop as state
   const urlParams = useParams();
+  const db = getDatabase(); //reference to the firebase DB
+
+  useEffect(() => {
+    //listen for changes to the database
+    const postRef = ref(db, "allPost");
+    //addEventListener('valueChange', () => {} )
+    const offFunction = onValue(postRef, (snapshot) => {
+      const allPosts = snapshot.val();
+
+      //convert object into an array
+      const objKeys = Object.keys(allPosts); //[Mpr5G, Mp55L, Mpr7s]
+      const valueArray = objKeys.map((key) => {
+        const valueAtKeyCopy = { ...allPosts[key], firebaseKey: key} 
+        return valueAtKeyCopy;
+      })
+
+      setMessageArray(valueArray);
+    })
+
+    //what to do when component is unmounted (removed)
+    const cleanup = () => {
+      offFunction();
+    }
+    return cleanup;
+  }, [])
 
   const addMessage = (msgText, msgUser, msgChannel) => {
     const newMessageObj = {
       userId: msgUser.uid,
-      userName: msgUser.username,
+      userName: msgUser.userName,
       userImg: "/img/"+msgUser.userName+".png", //hacky
       text: msgText,
       timestamp: Date.now(), //posted now
       channel: msgChannel
     }
-    const newMessageArray = [...messageArray, newMessageObj]; //spread to copy!
-    setMessageArray(newMessageArray);
+    
+    //write to firebase
+    const msgRef = ref(db, "allPost");
+    firebasePush(msgRef, newMessageObj) //adding to the array
+      .catch((err) => {
+        console.log(err.message);
+      })
+
+    // const newMessageArray = [...messageArray, newMessageObj]; //spread to copy!
+    // setMessageArray(newMessageArray);
   }
 
 
--- react-chat/src/index.js
+++ react-chat/src/index.js
@@ -5,9 +5,25 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+import { initializeApp } from "firebase/app";
+
 import App from './components/App';
 
 import { BrowserRouter } from 'react-router-dom';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyANuyQXu50nW7KvFbvFJ98cnjoDOJXllWs",
+    authDomain: "react-messenger-au21-a.firebaseapp.com",
+    databaseURL: "https://react-messenger-au21-a-default-rtdb.firebaseio.com",
+    projectId: "react-messenger-au21-a",
+    storageBucket: "react-messenger-au21-a.appspot.com",
+    messagingSenderId: "1082337143211",
+    appId: "1:1082337143211:web:bf7601fe31846b36f04aee"
+  };
+  
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
  
